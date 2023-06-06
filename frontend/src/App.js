c678b0eb0bf22fd37c1222fc6183dbedaed79b9e
bamtad <beamlak117@gmail.com>
2023-06-06 07:36:01 +0300

    B first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,27 @@
+import React from "react";
+import {  BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./Pages/loginPage";
+import Landingpage from "./Pages/Landingpage";
+import Dashboard from "./Pages/Dashboard";
+import Profile from "./Pages/Profile";
+import Notification from "./Pages/Notification";
+import Setting from "./Pages/Setting";
 function App() {
   return (
-    <h1>Hello,World! no</h1>
+    <div className="">
+      <Router>
+      <Routes>
+        <Route path="/" element={<Landingpage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+
+        <Route path="/profile" element={<Profile/> } />
+        <Route path="/notification" element={<Notification/>} />
+        <Route path="/setting" element={<Setting/>} />
+        
+      </Routes>
+      </Router>
+    </div>
   );
 }
 
