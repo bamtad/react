38b8fb382c3bed48518a482b82e3c689cccbd4af
bamtad <beamlak117@gmail.com>
2023-06-30 17:27:47 +0300

    merge solved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
+
 import LoginPage from "./Pages/loginPage";
 import Landingpage from "./Pages/Landingpage";
 import Dashboard from "./Pages/user/Dashboard";
 import Profile from "./Pages/user/Profile";
 import Notification from "./Pages/user/Notification";
 import Setting from "./Pages/user/Setting";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import firebase from "firebase/app";
-import "firebase/auth";
 
 import Document from "./Pages/user/Notification";
 import AdminDashboard from "./Pages/admin/AdminDashboard";
 import AdminProfile from "./Pages/admin/AdminProfile";
 import AdminNotification from "./Pages/admin/AdminNotification";
 import AdminSetting from "./Pages/admin/AdminSetting";
-import ComponentView from "./Pages/Document"
 import RegisterPage from "./Pages/registerPage";
 import { getAuth } from "firebase/auth";
 
@@ -41,13 +41,25 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Landingpage />} />
-          <Route path="/comp" element={<ComponentView />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
 
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/notification" element={<Notification />} />
-          <Route path="/setting" element={<Setting />} />
+          <Route
+            path="/dashboard"
+            Component={withAuthentication(Dashboard)}
+          />
+          <Route
+            path="/profile"
+            Component={withAuthentication(Profile)}
+          />
+          <Route
+            path="/notification"
+            Component={withAuthentication(Notification)}
+          />
+          <Route
+            path="/setting"
+            Component={withAuthentication(Setting)}
+          />
 
           <Route
             path="/admindashboard"
@@ -72,10 +84,6 @@ function App() {
   
   />
 
-          <Route path="/Admindashboard" element={<AdminDashboard />} />
-          <Route path="/Adminprofile" element={<AdminProfile />} />
-          <Route path="/Adminnotification" element={<AdminNotification />} />
-          <Route path="/Adminsetting" element={<AdminSetting />} />
         </Routes>
       </Router>
     </div>
--- frontend/src/Pages/loginPage.js
+++ frontend/src/Pages/loginPage.js
@@ -65,7 +65,7 @@ function LoginPage() {
 
   return (
     <div>
-      <section className="h-screen  flex flex-col md:flex-row justify-center space-y-20 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+      <section className="h-screen w-full flex flex-col md:flex-row justify-center space-y-20 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
         <div className="md:w-1/3 max-w-sm">
           <img
             src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
